70e0f1fe23b35931b39dde84876c8b88d921681c
tyfoster97 <tyfoster@outlook.com>
2020-04-03 01:08:12 -0700

    Have main process function calls

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import api
+import fish
+import misc
+import insect as ins
+import inventory as inv
 
-api.load_inv()
+inv.read()
 
 cmd = input(":").split(" ", 1)
 #ai -> add insect
@@ -18,11 +21,34 @@ cmd = input(":").split(" ", 1)
 #su-mi -> edit misc
 
 while(cmd[0] != "q"):
-    if len(cmd) > 1:
-        api.do_cmd(cmd[0], cmd[1])
-    else:
-        api.do_cmd(cmd[0])
-    cmd = input(":").split(" ", 1)
+    if cmd[0] == "l":
+        print(inv.all())
+    elif cmd[0] == "su-in":
+        ins.edit()
+    elif cmd[0] == "su-fi":
+        fish.edit()
+    elif cmd[0] == "su-mi":
+        misc.edit()
+    elif cmd[0] == "ai":
+        inv.add(cmd[1], ins.search(cmd[1]))
+    elif cmd[0] == "af":
+        inv.add(cmd[1], fish.search(cmd[1]))
+    elif cmd[0] == "am":
+        inv.add(cmd[1], misc.search(cmd[1]))
+    elif cmd[0] == "si":
+        print(ins.search(cmd[1]))
+    elif cmd[0] == "sf":
+        print(fish.search(cmd[1]))
+    elif cmd[0] == "sm":
+        print(misc.search(cmd[1]))
+    elif cmd[0] == "ci":
+        print(inv.compare(ins.search(cmd[1])))
+    elif cmd[0] == "cf":
+        print(inv.compare(fish.search(cmd[1])))
+    elif cmd[0] == "cm":
+        print(inv.compare(misc.search(cmd[1])))
+    elif cmd[0] == "r":
+        inv.remove(cmd[1])
 
 if(cmd[0] == "q"):
-    api.write_inv()
+    inv.write()
